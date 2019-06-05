1d2b3a2961ae31c8a63d74c5ec6c92d9445e5fad
Allison King <allisonjuliaking@gmail.com>
2019-06-05 13:20:19 -0400

    add frontend build

---
+++ frontend/build/precache-manifest.a40732bc2cda097fb9e54fba16a87009.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255a1b7d39424fdcc01943421abcaf3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4179fd9cf96887efcdf0",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a379c2fe5bbf44827898",
+    "url": "/static/js/2.cfe06d37.chunk.js"
+  },
+  {
+    "revision": "4179fd9cf96887efcdf0",
+    "url": "/static/js/main.c7597370.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
